37505015b4fffb488b54aae62b42d743dc449f1e
Aaron-Doran <wesley.gossett@gmail.com>
2019-11-04 17:59:58 -0500

    Fixed bug with score updating for boths teams when clicking any button. Added Extra Point, 2 Point Conversion, & Reset buttons to project

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@
 import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import { restElement } from "@babel/types";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [score, setScore] = useState(0);
+  const [homeScore, setHomeScore] = useState(0);
+  const [awayScore, setAwayScore] = useState(0);
+  const reset = () => {
+    setHomeScore(0);
+    setAwayScore(0);
+  };
 
 
   return (
@@ -17,12 +23,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">{score}</div>
+            <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{score}</div>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
@@ -30,12 +36,19 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button onClick={() => setScore(score + 6)} className="homeButtons__touchdown">Home Touchdown</button>
-          <button onClick={() => setScore(score + 1)} className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick={() => setHomeScore(homeScore + 6)} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick={() => setHomeScore(homeScore + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick={() => setHomeScore(homeScore + 1)} className="homeButtons__extraPoint">Home Extra Point</button>
+          <button onClick={() => setHomeScore(homeScore + 2)} className="homeButtons__twoPointConversion">2 Point Conversion</button>
         </div>
         <div className="awayButtons">
-          <button onClick={() => setScore(score + 6)} className="awayButtons__touchdown">Away Touchdown</button>
-          <button onClick={() => setScore(score + 1)} className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick={() => setAwayScore(awayScore + 6)} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={() => setAwayScore(awayScore + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick={() => setAwayScore(awayScore + 1)} className="awayButtons__extraPoint">Away Extra Point</button>
+          <button onClick={() => setAwayScore(awayScore + 2)} className="awayButtons__twoPointConversion">2 Point Conversion</button>
+        </div>
+        <div className="restButton">
+          <button onClick={reset} className='resetButtons__reset'>Reset Game</button>
         </div>
       </section>
     </div>
